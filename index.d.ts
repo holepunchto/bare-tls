43625f2ea570085084457452b8585d9131f544f6
yasser <dev@yasser.email>
2025-02-03 13:05:20 -0300

    Add TypeScript typings (#1)

---
+++ index.d.ts
@@ -0,0 +1,29 @@
+import { Duplex, DuplexEvents } from 'bare-stream'
+import constants from './lib/constants'
+import TLSError from './lib/errors'
+
+export { constants, TLSError as errors }
+
+export interface TLSSocketEvents extends DuplexEvents {
+  connect: []
+}
+
+export interface TLSSocketOptions {
+  allowHalfOpen?: boolean
+  cert?: ArrayBufferView
+  eagerOpen?: boolean
+  host?: string
+  isServer?: boolean
+  key?: ArrayBufferView
+}
+
+export interface TLSSocket<M extends TLSSocketEvents> extends Duplex<M> {
+  readonly socket: Duplex
+  readonly encrypted: true
+}
+
+export class TLSSocket<M extends TLSSocketEvents> extends Duplex<M> {
+  constructor(socket: Duplex, opts?: TLSSocketOptions)
+}
+
+export { TLSSocket as Socket }
